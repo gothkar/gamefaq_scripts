dd8796f4dd50d12a497e530c8ad2d9615feb699b
Will Sheppard <will.sheppard.uk@gmail.com>
2022-03-07 13:46:36 +0000

    Loads of refactoring

--- script.sh
+++ script.sh
@@ -1,63 +1,73 @@
 #!/bin/bash
+set -eu
+IFS=$'\n\t'
 
-# Script to download all text files from gamefaqs.com
+#set -vx # Uncomment for debug
 
-# TODO:
-# - s/999-all/games_page.html/g
-# - wget -O games_page${i}.html http://...etc...page=$i
-# - s/"$i"/${i}/
+# Script to download all FAQs from gamefaqs.com
+
+HOST="https://gamefaqs.gamespot.com"
 
 WAIT_TIME=2
+LIVE=true # false will prevent live http calls
 
-mkdir -p .working # to keep temporary files tidy
+WD=".work"      && mkdir -p $WD     # keep temporary files tidy in a Working Directory
+OUTPUT="output" && mkdir -p $OUTPUT # keep output files in one place
 
 # Fetch list of systems
-wget -O systems.html 'https://gamefaqs.gamespot.com/games/systems'
-cat systems.html | grep 'href' | sed 's/.\+href="\([^"]\+\)".\+/\1/' | grep -Ev '^(/user|/games|/a/|/new|http)' | sort | uniq > systems_list.txt
-mv systems.html .working
+$LIVE && wget -O $WD/systems.html "$HOST/games/systems"
+sleep $WAIT_TIME
+cat $WD/systems.html | grep 'href' | sed 's#.\+href="/\([^"]\+\)".\+#\1#' | grep -Ev '^(user|games|a/|new|http)' | grep -v '<' | sort | uniq > systems_list.txt
 
 # Loop over systems
-cat systems_list.txt | head -1 | while read system
+cat systems_list.txt | head -1 | while read line
 do
+    system=$line
 
-    # Fetch list of games
-    wget -O games0.html https://gamefaqs.gamespot.com/"$system"/category/999-all
+    # Fetch first page of games
+    $LIVE && wget -O $WD/games0.html $HOST/"$system"/category/999-all
     sleep $WAIT_TIME
 
     # Extract number of pages of games
-    maxpage=$(cat games0.html | grep "<option value" | awk -F= 'BEGIN { max = -1 } { if ($3 > max) { max = $3; line = $0 } } END { print $line }' | grep -o '".*"' | sed 's/"//g')
-    mv games0.html .working
+    maxpage=$(cat $WD/games0.html | grep "<option value" | awk -F= 'BEGIN { max = -1 } { if ($3 > max) { max = $3; line = $0 } } END { print $line }' | grep -o '".*"' | sed 's/"//g')
+    echo maxpage = '$maxpage'
 
     # Extract game links from first page
-    cat games0.html | grep "/$system/" | grep "Guides" | grep "<td class=" | awk '{$1=$1};1' | cut -c28- | sed 's/faqs.*/faqs/' | awk '$0="https://gamefaqs.gamespot.com"$0' >> game_links.txt
+    cat $WD/games0.html | grep "/$system/" | grep "Guides" | grep "<td class=" | awk '{$1=$1};1' | cut -c28- | sed 's/faqs.*/faqs/' | awk '$0="'$HOST'"$0' >> game_links.txt
 
     maxpage=1 # DEBUG
     # Loop over each page, collecting all game links
     for (( i=1; i<="$maxpage"; i++ ))
     do
-        wget https://gamefaqs.gamespot.com/"$system"/category/999-all?page="$i"
-        cat 999-all@page="$i" | grep "/$system/" | grep "Guides" | grep "<td class=" | awk '{$1=$1};1' | cut -c28- | sed 's/faqs.*/faqs/' | awk '$0="https://gamefaqs.gamespot.com"$0' >> game_links.txt
-        rm  999-all@page="$i"
+        $LIVE && wget -O $WD/games"$i".html $HOST/"$system"/category/999-all?page="$i"
         sleep $WAIT_TIME
+        cat $WD/games"$i".html | grep "/$system/" | grep "Guides" | grep "<td class=" | awk '{$1=$1};1' | cut -c28- | sed 's/faqs.*/faqs/' | awk '$0="'$HOST'"$0' >> game_links.txt
     done
 
     # Loop over each game, collecting all FAQs
     cat game_links.txt | head -1 | while read line
     do
-        wget "$line"
-        cat faqs | grep "<li data-url=" | sed -n '/<div class/q;p' | awk '{$1=$1};1' | cut -c15- | sed 's/..$//' | awk '$0="https://gamefaqs.gamespot.com"$0' >> faq_links.txt
-        rm faqs
+        $LIVE && wget -O $WD/game.html "$line"
         sleep $WAIT_TIME
+        cat $WD/game.html | grep "<li data-url=" | sed -n '/<div class/q;p' | awk '{$1=$1};1' | cut -c15- | sed 's/..$//' | awk '$0="'$HOST'"$0' >> faq_links.txt
     done
 
-    # Download each FAQ
+    # Loop over each FAQ, download it
+    # Notes:
+    # - Example URL: https://gamefaqs.gamespot.com/3do/314778-the-11th-hour/faqs/6221
+    # - The system (3do) is just a label; The same FAQ content may be found labelled with each system for which it is valid.
+    #   ...this means there will be duplicate content downloaded.
+    #   ...this is tolerated for now, only because we need to maintain the system labels somehow.
+    # - The first number (314778) uniquely identifies the game.
+    # - The second number (6221) uniquely identifies the FAQ document.
     cat faq_links.txt | head -1 | while read line
     do
-        wget -O faqs "$line"
-        name=$(echo "$line" | cut -c30- | sed 's#/#\_#g' | cut -c2-)
-        cat faqs | sed '/faqtext/,$!d' | sed '1d' | sed '/\/pre/,$d' >> "$name".txt
-        rm faqs
+        $LIVE && wget -O $WD/faq.html "$line"
         sleep $WAIT_TIME
+        name=$(echo "$line" | sed 's#'$HOST'/##' | sed 's#faqs/##' | sed 's#/#\_#g')
+        cat $WD/faq.html | sed '/faqtext/,$!d' | sed '1d' | sed '/\/pre/,$d' >> "$OUTPUT/$name".txt
     done
 
+    # Clean up
+    rm -r .working
 done
